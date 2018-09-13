01cce2d0f563ab7c3c6c5fde81c895cf30e6d43d
Piezoelectric <gerald.hu.99@gmail.com>
2018-09-13 07:07:11 +0000

    Part 1 finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,20 @@
 
 def sum arr
   # YOUR CODE HERE
+  return arr.sum #This is the copout answer but hey, it's supported in 2.4
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  arr = arr.sort
+  arr = arr.reverse #Easiest way to get max 2 elements: sort the arr
+  return arr[0,2].sum #Start at indx 0, retrieve 2 elements
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arrayOfPairs = arr.combination(2).to_a #all combos of len n of elements from the array -> to_array
+  arrayOfPairSums = arrayOfPairs.map{|x| x.sum} #take each element (a pair), sum it
+  return arrayOfPairSums.include? n
 end
 
 # Part 2
